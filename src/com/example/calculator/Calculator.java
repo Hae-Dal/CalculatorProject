45f4dff7aa05c0b985bb5f45457d2e397dd11eca
HAEDAL-IRLAB <jyh4282002@naver.com>
2024-09-09 12:32:17 +0900

    [UPDATE] 도전 기능 완성

--- src/com/example/calculator/Calculator.java
+++ src/com/example/calculator/Calculator.java
@@ -6,39 +6,43 @@ import java.util.HashMap;
 public class Calculator {
 
     // 연산 결과를 저장하는 컬렉션 타입 필드
-    private ArrayList<HashMap<String, String>> calcResult = new ArrayList<>();
+    private ArrayList<Double> calcResult = new ArrayList<>();
 
-    public ArrayList<HashMap<String, String>> getCalcResult() {
+    public ArrayList<Double> getCalcResult() {
         return calcResult;
     }
 
-    public void setCalcResult(ArrayList<HashMap<String, String>> calcResult) {
+    public void setCalcResult(ArrayList<Double> calcResult) {
         this.calcResult = calcResult;
     }
 
-    public double calc(int first, int second, char op) {
-        HashMap<String, String> result = new HashMap<>();
-        result.put("expr", first + String.valueOf(op) + second);
-        calcResult.add(result);
+    // 연산 결과 저장 필드의 가장 첫번째 데이터 삭제
+    public void deleteCalcResultFirst() {
+        if (!calcResult.isEmpty()) {
+            calcResult.remove(0);
+        }
+    }
+
+    public double calc(int first, int second, OperatorType op) {
 
         switch (op) {
-            case '+' : {
-                result.put("result", String.valueOf((double)first + second));
+            case Add: {
+                calcResult.add((double)first + second);
                 return first + second;
             }
-            case '-' : {
-                result.put("result", String.valueOf((double) first - second));
+            case Subtract: {
+                calcResult.add((double)first - second);
                 return first - second;
             }
-            case '*' : {
-                result.put("result", String.valueOf((double) first * second));
+            case Multiply: {
+                calcResult.add((double)first * second);
                 return first * second;
             }
-            case '/' : {
+            case Divide: {
                 if (second == 0) {
                     throw new IllegalArgumentException("0으로는 나눌 수 없습니다.");
                 } else {
-                    result.put("result", String.valueOf((double) first / second));
+                    calcResult.add((double)first / second);
                     return (double) first / second;
                 }
             }
